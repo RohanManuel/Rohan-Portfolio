b12d8b4d272312f8198c3fd75b9f0edd643aca98
Rohan Manuel . A <130838957+RohanManuel@users.noreply.github.com>
2025-02-19 16:09:37 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/RohanManuel",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1r6jxauwtKGsF1OjHz9S9nDHgk8KJTNji/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/rohan-manuel-a-89610925a/",
   twitter: "https://x.com/Madmanatrise1",
   insta: "https://www.instagram.com/mesmerizer_king_?igsh=dzBvb3l0eGo2YWRu",
